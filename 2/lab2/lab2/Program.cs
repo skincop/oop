df2a2951bb0a984daf999cd5c4fc9e2414d014be
skincop <skincop@gmail.com>
2021-10-01 15:45:34 +0300

    Add 2-4 lab

---
+++ 2/lab2/lab2/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Text;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region инициализация
+            byte @byte = 250;                //1
+            sbyte @sbyte = -120;              //1
+            short @short = -3200;            //2
+            ushort @ushort = 65500;          //2
+            int @int = 2100000000;           //4
+            uint @uint = 4200000;
+            long @long = 90000000000;       //8
+            ulong @ulong = 18000000000;
+            float @float = 3.3f;            //4
+            double @double = 1.7;           //8
+            char @char = 'a';               //2
+            string @string = "Hello";
+            bool @bool = true;
+            @int = Convert.ToInt32(@float);
+            //1
+            Console.WriteLine("byte: {0}, sbyte: {1} ,short: {2}", @byte, @sbyte, @short);
+            #endregion
+            #region преобразование
+            @long = @int;
+            @short = @byte;
+            @int = @sbyte;
+            @float = @ushort;
+            @double = @float;
+            /////////////////////// явное
+            @sbyte = (sbyte)@byte;
+            @short = (short)@int;
+            @byte = (byte)@double;
+            @uint = (uint)@int;
+            @ushort = (ushort)@ulong;
+            //упаковка и распаковка
+            int packing = 123;
+            object o = packing;
+            int unpucking = (int)o;
+
+            #endregion
+            #region ввод вывод
+            Console.WriteLine("byte: {0}, sbyte: {1} ,short: {2}", @byte, @sbyte, @short);
+            Console.WriteLine("Type number [0,255]   ");
+            byte a = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine(a);
+            var myVar = 10.0;
+            float fl = 3.4f;
+            myVar = fl;
+            #endregion
+            Console.ReadKey();
+            
+            Console.Clear();
+            #region строки
+            Console.WriteLine("Type 2 string");
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            //сравнение
+            Console.Write("Is equal? ");
+            if (String.Equals(str1, str2)) Console.WriteLine("equal");
+            else Console.WriteLine("diffirent");
+            Console.WriteLine("String concat: {0}", String.Concat(str1, str2));
+            //провкера на содержание 2 строки в 1
+            Console.Write("Does the str1 has str2: ");
+            if (str1.Contains(str2)) Console.WriteLine("Yeah");
+            else Console.WriteLine("No");
+            //Убрать 2 первых символа в строке
+            string text = "Hello! How do you do?";
+            Console.WriteLine("Delete 2 symbols at str {0}", text.Substring(2));
+            //вывод по словам
+            Console.Write("String split: ");
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++) Console.Write(", {0} ", words[i]);
+            //Соеденение двух,убрать все после 6 символа
+            Console.WriteLine("");
+            Console.WriteLine("Replace str {0}",text.Insert(6, str2));
+            Console.WriteLine("Delete after 6 {0}", text.Remove(6));
+            //интерполяция строк
+            string name = "Yan";
+            Console.WriteLine($"{name} is 18 years old");
+            string empty = "";
+            string _null = null;
+            Console.WriteLine("Is Null or Empty: {0}", String.IsNullOrEmpty(empty));
+            Console.WriteLine("Is Null or Empty: {0}", String.IsNullOrEmpty(_null));
+            StringBuilder sb = new StringBuilder("Привет мир меня зовут Ян");
+            Console.WriteLine(sb.Remove(6, 4));
+            Console.WriteLine(sb.Replace("Ян", "Yan"));
+            Console.WriteLine(sb.AppendFormat("123123"));
+
+            #endregion
+            Console.ReadKey();
+            Console.Clear();
+            #region массив
+            // двумерный массив 
+            int[,] array = new int[3, 3];
+            Random ran = new Random();
+
+            // Инициализируем данный массив
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    array[i, j] = ran.Next(1, 15);
+                    Console.Write("{0}\t", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            //вывод массива строк с длинной
+            string[] weekDays = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+            Console.WriteLine("Length of array {0}", weekDays.Length);
+            for (int i = 0; i < weekDays.Length; i++)
+            {
+                Console.Write("{0} ,", weekDays[i]);
+            }
+            Console.WriteLine(" ");
+            //замена элемента  в строке
+            Console.WriteLine("Type position (min 1)");
+            int pos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type new str");
+            string newStr = Console.ReadLine();
+            weekDays[pos - 1] = newStr;
+            for (int i = 0; i < weekDays.Length; i++)
+            {
+                Console.Write("{0} ,", weekDays[i]);
+            }
+            //ступенчатый массив
+            int[][] myArr = new int[3][];
+            myArr[0] = new int[2];
+            myArr[1] = new int[3];
+            myArr[2] = new int[4];
+            int m = 2;
+            Console.WriteLine("");
+            Console.WriteLine("Type value: ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    myArr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+                m++;
+            }
+            m = 2;
+            Console.Clear();
+            //вывод ступенчатого массива
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("{0}   ", myArr[i][j]);
+                }
+                Console.WriteLine("");
+                m++;
+            }
+            var unkArray = new object[] { 5, 10, 23, 16, 8 };
+            var unkStr = "11111";
+            Console.WriteLine("{0}", unkStr);
+            for (int i=0;i<unkArray.Length;i++)
+            {
+                Console.Write("{0}  ",unkArray[i]);
+            }
+
+
+            Console.WriteLine("");
+            #endregion
+            Console.ReadKey();
+            Console.Clear();
+            #region кортежи
+            (int, string, char, string, ulong) person = (18, "Yan", 'A', "Vysotskiy", 30000000);
+            (int, string, char, string, ulong) person1 = (18, "Yan", 'A', "Vysotskiy", 31000000);
+            Console.WriteLine(person);
+            Console.WriteLine("{0}  {1}  {2}", person.Item1, person.Item3, person.Item4);
+
+
+            string newStr1 = "string";
+            int[] newArray = new int[3] {1,2,3 };
+            var cortage = func(newStr1,newArray);
+            Console.WriteLine(cortage);
+            bool tuple = (person == person1);
+            Console.WriteLine(tuple);
+            object tuplePerson = person.Item5;
+            int intTuplePerson = person.Item1;
+            func1();
+            func2();
+            #endregion
+            #region функция возврат кортежа
+
+
+            //функция возврат кортеж
+
+            static (int,int,int,string) func (string str, int[] array)
+
+            {
+                
+                int min = 1000000,max = 0,sum=0;
+                for (int i=0;i<array.Length;i++)
+                {
+                    sum += array[i];
+                    if (array[i] > max) max = array[i];
+                    if (array[i] < min) min = array[i];
+
+                }
+                return (min, max, sum, str.Substring(0, 1));
+            }
+            #endregion
+            #region функции checked
+            void func1 ()
+            {
+                try
+                {
+                    byte a = 255,b = 1,result;
+                    checked
+                    {
+                        result = (byte)(a + b);
+                        Console.WriteLine("a + b = " + result);
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.Write("Переполнение\n\n");
+                }
+            }
+
+            void func2()
+            {
+               try
+            {
+                byte a = 255, b = 1, result;
+                unchecked
+                {
+                    result = (byte)(a + b);
+                    Console.WriteLine("a + b = " + result);
+                }
+            }
+                catch (OverflowException)
+            {
+                Console.Write("Переполнение\n\n");
+               }
+            }
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+                      Console.ReadKey();
+
+
+        }
+    }
+}
+
---
+++ 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    partial class Product
+    {
+        private readonly int id;
+        private static int sum;
+        public static int _id = 1;
+        private string name = "";
+        private int upc;
+        private string manuf = "";
+        private int price;
+        private string data = "";
+        private int quanity;
+        const string city = "Minsk";
+        public int Upc
+        {
+            set
+            {
+                upc = value;
+            }
+            get
+            {
+                return upc;
+            }
+        }
+        public int Price
+        {
+            set
+            {
+                if (value > 0 && value < 1000)
+                    price = value;
+            }
+            get
+            {
+                return price;
+            }
+        }
+        public string Name
+        {
+            set
+            {
+                if (value != null && value != "")
+                    name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public string Manuf
+        {
+            set
+            {
+                if (value != null && value != "")
+                    manuf = value;
+            }
+            get
+            {
+                return manuf;
+            }
+        }
+        public string Data
+        {
+            set
+            {
+                if (value != null && value != "")
+                    data = value;
+            }
+            get
+            {
+                return data;
+            }
+        }
+        public int Quanity
+        {
+            set
+            {
+                if (value != 0 && value < 1000)
+                    quanity = value;
+            }
+            get
+            {
+                return quanity;
+            }
+        }
+        public Product(string _name = "unknown", int _upc = -1, string _manuf = "noname", int _price = 999999, string _data = "00.00.0000", int _quanity = -1)
+        {
+            id++;
+            this.id = _id;
+            if (_name != "" && _name != null)
+                this.name = _name;
+            else this.name = "unknown";
+            if (_upc != 0)
+                this.upc = _upc;
+            else this.upc = -1;
+            if (_manuf != "" && _manuf != null)
+                this.manuf = _manuf;
+            else this.manuf = "noname";
+            if (_price != 0)
+                this.price = _price;
+            else this.price = 999999;
+            if (_data != "" && _data != null)
+                this.data = _data;
+            else this.data = "00.00.0000";
+            if (_quanity > 0)
+                this.quanity = _quanity;
+            else this.quanity = 0;
+
+
+
+        }
+        public Product(string _name)
+        {
+
+        }
+        //public Product()
+        //{
+
+        //}
+        static Product()
+        {
+
+        }
+        private Product()
+        {
+            Console.WriteLine(new string('!', 20));
+        }
+
+
+        public void Getinfo()
+        {
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(this.id + "\n" + this.name + "\n" + this.upc + "\n" + this.manuf + "\n" + this.price + "\n" + this.data + "\n" + this.quanity);
+            Console.WriteLine(new string('-', 50));
+
+        }
+        public void Usless()
+        {
+        }
+        public static int Yan= 10000234;
+        static void InfoClass()
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine(Yan);
+            Console.WriteLine(_id);
+            Console.WriteLine("-------------------------");
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Product m = obj as Product;
+            if (m as Product == null)
+                return false;
+
+            return m.id == this.id && m.name == this.name && m.upc == this.upc && m.quanity == this.quanity;
+        }
+        public override int GetHashCode()
+        {
+            return this.id;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + this.id + "name: " + this.name;
+        }
+
+
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[6];
+            products[0] = new Product("pen",1111,"maped",10,"26.06.2013",25);
+            products[1] = new Product("pencil",2222,"erich",15,"01.11.2013",25);
+            products[2] = new Product("rubber",3333,"maped",30,"26.09.2015",10);
+            products[3] = new Product("pen",4444,"erich",30,"16.10.2019",10);
+            products[4] = new Product("rubber",5555,"maped",100,"16.10.2019",3);
+            products[5] = new Product("book",6666,"ozzz",200,"10.16.2021",100);
+            products[0].Getinfo();
+            products[0].Usless();
+            void Findname ()
+            {
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Type a name of product");
+                string find = Console.ReadLine();
+                for (int i = 0; i < products.Length; i++)
+                {
+                    string name = products[i].Name;
+                    if (find == name)
+                        products[i].Getinfo();
+                }
+            }
+            void Findprice()
+            {
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Type a name of product");
+                string find = Console.ReadLine();
+                Console.WriteLine("Type price of product");
+                int price = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < products.Length; i++)
+                {
+                    if (find == products[i].Name && price >= products[i].Price)
+                        products[i].Getinfo();
+                }
+            }
+            Findname();
+            Findprice();
+            var ruler = new { name="ruler",price=10,manuf="ozzz", quanity=5 };
+            Console.WriteLine(ruler.manuf);
+
+
+
+
+
+
+        }
+    }
+}
