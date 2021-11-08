36d63fe87fae1caebfa660679467c193491ce0d5
skincop <skincop@gmail.com>
2021-11-08 10:30:37 +0300

    add 7

---
+++ 10/lab10/lab10/Computer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Computer<T> : IList<T>
+    {
+        public T Id { get; set; }
+        public string CpuName { get; set; }
+        public string GpuName { get; set; }
+        public int RamValue { get; set; }
+
+      
+
+        public Computer() { }
+        public Computer(int id, string cpuName, string gpuName, int ramValue)
+        {
+            object o = id;
+            Id = (T)o;
+            CpuName = cpuName;
+            GpuName = gpuName;
+            RamValue = ramValue;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Id {Id} CPU {CpuName} GPU {GpuName} Ram {RamValue}";
+        }
+
+
+        #region interface
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ 10/lab10/lab10/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Lab10
+{
+    class Program 
+    {
+        private static void Obs_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine("Элемент добавлен ");
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Элемент удален ");
+            }
+            else
+            {
+                Console.WriteLine("Нет добавленных/удаленных элементов");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region HashSet<Computer>
+
+            HashSet<Computer<int>> computers = new HashSet<Computer<int>>();
+
+            Computer<int> comp1 = new Computer<int>(1, "core i9", "RTX 3060", 64);
+            Computer<int> comp2 = new Computer<int>(2, "core i5", "GT 1050", 16);
+            Computer<int> comp3 = new Computer<int>(3, "core i7", "GTX 1660", 32);
+
+
+            computers.Add(comp1);
+            computers.Add(comp2);
+            computers.Add(comp3);
+            computers.Add(comp1);
+
+
+            foreach (var item in computers)
+            {
+                Console.WriteLine($"{item.ToString()}");
+            }
+
+            computers.Remove(comp2);
+
+            Console.WriteLine(new string('-', 50));
+            foreach (var item in computers)
+            {
+                Console.WriteLine($"{item.ToString()}");
+            }
+
+            HashSet<Computer<int>> computers1 = new HashSet<Computer<int>>();
+            computers1.Add(comp2);
+
+            computers.UnionWith(computers1);
+
+            Console.WriteLine(new string('-', 50));
+            foreach (var item in computers)
+            {
+                Console.WriteLine($"{item.ToString()}");
+            }
+            #endregion
+            #region HashSet<int>
+
+            HashSet<int> integer = new HashSet<int>();
+            Console.WriteLine(new string('-',50));
+
+            for(int i=0;i<10;i++)
+            {
+                int rand = new Random().Next(0, 5);
+                integer.Add(rand);
+                Console.Write($"{rand} ");
+            }
+
+            Console.WriteLine("");
+
+            foreach(int item in integer)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine("");
+
+
+
+            #endregion
+
+            #region event
+            ObservableCollection<Computer<int>> Col = new ObservableCollection<Computer<int>>(); //ObservableCollection как list 
+            Col.CollectionChanged += Obs_CollectionChanged;
+            Console.WriteLine(new string('-',50));
+            Col.Add(comp1);
+            Col.Add(comp2);
+            Col.Remove(comp1);
+            Col.Add(comp2);
+
+            foreach (Computer<int> item in Col)
+            {
+                Console.WriteLine("");
+            }
+            #endregion
+
+
+        }  
+    }
+}
