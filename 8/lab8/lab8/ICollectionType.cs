fd7d2b1ca849e57aa5227ea602834ee588924b4f
skincop <skincop@gmail.com>
2021-11-08 10:05:55 +0300

    add 7-12

---
+++ 8/lab8/lab8/ICollectionType.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab8
+{
+    public interface ICollectionType<T> where T : class
+    {
+        void Add(T myList);
+        void Insert(int index, T myList);
+        void RemoveAt(int index);
+        void Clear();
+        void Show();
+        void Print();
+        public void SerializeAndSave();
+    }
+}
